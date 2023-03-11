fe3da0b40c529789d47e75d89589f9480db22bf3
Gabboooo <gabriele.bianchi21@gmail.com>
2023-03-11 12:04:06 +0100

    git init

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+port = 8080;
+
+const places = JSON.parse(fs.readFileSync('./places.json'));
+
+console.log()
+
+const express = require("express")
+const app = express()
+
+app.get('', (req, res)=>{
+    res.send('go to /places!');
+})
+
+app.get('/places', (req, res)=>{
+    res.json(places)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
