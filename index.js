590580f36225c35c94a7930340ec351944540d98
Gabriele Bianchi <gabriele.bianchi21@gmail.com>
2023-03-16 12:35:41 +0100

    some pg stuff

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const fs = require('fs');
-port = process.env.PORT || 8080;
-
+const { Pool } = require('pg')
+//constants and json
+const port = process.env.PORT || 8080;
 const places = JSON.parse(fs.readFileSync('./places.json'));
-
-console.log()
-
+//express
 const express = require("express")
 const app = express()
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+  });
 
 app.get('', (req, res)=>{
     res.statusCode = 200;
@@ -19,6 +28,15 @@ app.get('/places', (req, res)=>{
     res.json(places)
 })
 
+//gets all the users id existing in the database (useless atm)
+app.get('/users', (req,res)=>{
+    res.statusCode = 200;
+    pool.query('SELECT * from users', (err,result)=>{
+        res.json(result.rows)
+    })
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server listening on port ${port}`)
   })
+
