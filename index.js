d904b9f464f4c183bd0a3aaa46931893951872d1
Gabboooo <gabriele.bianchi21@gmail.com>
2023-03-23 19:16:46 +0100

    /history POST method

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // dotenv import config
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
 
 // other imports
 const fs = require('fs');
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 const { createHash } = require('crypto');
-const express = require("express")
+const express = require("express");
 
 // constants and json
 const port = process.env.PORT || 8080;
@@ -15,6 +15,11 @@ const places = JSON.parse(fs.readFileSync('./places.json'));
 // express init
 const app = express()
 app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 
 // hash function, uses crypto
 function hash(string) {
@@ -33,33 +38,45 @@ const pool = new Pool({
 // EXPRESS ROUTES
 app.get('', (req, res)=>{
     res.statusCode = 200;
-    res.sendFile(__dirname + 'index.html')
+    res.sendFile(__dirname + 'index.html');
 })
 
 app.get('/places', (req, res)=>{
     res.statusCode = 200;
     pool.query('SELECT * from places', (err,result)=>{
-        res.json(result.rows)
+        res.json(result.rows);
     })
 })
 
 app.get('/get_user', (req,res) =>{
-    let email = req.query.email
+    let email = req.query.email;
     pool.query('SELECT user_id FROM users WHERE user_mail = $1::text', [email], (err,result) =>{
         if(result.rows.length == 0){
             pool.query('INSERT INTO users (user_mail, user_id) VALUES ($1::text, DEFAULT)', [email], (err,result)=>{
                 pool.query('SELECT user_id FROM users WHERE user_mail = $1::text', [email], (err,result)=>{
-                    res.json(result.rows[0])
+                    res.json(result.rows[0]);
                 })
             })
         }
         else{
-            res.json(result.rows[0])
+            res.json(result.rows[0]);
         }
     })
 })
 
+app.post('/history', (req,res)=>{
+    const body = req.body;
+    pool.query('INSERT INTO history (userid, placeid) VALUES ($1, $2)', [body['userid'], body['placeid']],(err, result)=>{
+        if(err){
+            res.status(400).send('Error occured, parameters may be wrong.\nParameters expected: userid, placeid.\nIf the problem persists then use another API :\)')
+            console.log(err);
+        }
+        res.status(201).send('Success')
+        // console.log(result)
+    })
+})
+
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`);
   })
 
